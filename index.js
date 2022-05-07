68cad225c5646d41d5d06858a17e7a480e086edf
Kenny48620 <kennyleetrade@gmail.com>
2022-05-06 17:33:33 -0700

    chexk

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 4002;
 
 //middlewares
 app.use(cors({
-    origin: "https://kenny48620.github.io",
+    origin: "https://Kenny48620.github.io",
     credentials: true
 }));
 app.use(express.json());
