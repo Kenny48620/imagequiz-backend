d2869a05eb9a9860e0b6b8cf3f6cb57e96874f17
Kenny48620 <kennyleetrade@gmail.com>
2022-05-11 18:20:35 -0700

    chexk

--- data_access/store.js
+++ data_access/store.js
@@ -2,7 +2,9 @@ const bcrypt = require('bcrypt');
 const { Pool } = require('pg');
 require('dotenv').config();
 let { quizzes } = require('../temp_store/data');
-const flowers = require('../temp_store/flowers');
+//const flowers = require('../temp_store/flowers');
+
+var format = require('pg-format');
 
 const connectionString =
     `postgres://${process.env.USERNAME}:${process.env.PASSWORD}@${process.env.HOST}:${process.env.DATABASEPORT}/${process.env.DATABASE}`;
@@ -13,75 +15,40 @@ const connection = {
     connectionString: process.env.DATABASE_URL ? process.env.DATABASE_URL : connectionString,
     ssl: { rejectUnauthorized: false }
 }
-const pool = new Pool(connection);
 
+const pool = new Pool(connection);
 
 let store = {
 
     addCustomer: (name, email, password) => {
         const hash = bcrypt.hashSync(password, 10);
-        return pool.query('insert into imagequiz.customer (name, email, password) values ($1 , $2, $3)', [name, email, hash]);
-        //customers.push({id: 1, name: name, email: email, password: hash});
+        return pool.query('INSERT INTO imagequiz.customer (name, email, password) VALUES ($1, $2, $3)', [name, email, hash]);
     },
 
     login: (email, password) => {
-        return pool.query('select name, email, password from imagequiz.customer where email = $1', [email])
+        return pool.query('SELECT id, name, email, password FROM imagequiz.customer WHERE email = $1', [email])
             .then(x => {
                 if (x.rows.length == 1) {
                     let valid = bcrypt.compareSync(password, x.rows[0].password);
                     if (valid) {
-                        return { valid: true };
+                        return { valid: true, user: {id: x.rows[0].id, username: x.rows[0].email}};
                     } else {
-                        return { valid: false, message: 'Credentials are not valid.' }
+                        return { valid: false, message: 'Credentials are not valid.' };
                     }
                 } else {
-                    return { valid: false, message: 'Email not found.' }
+                    return { valid: false, message: 'Email not found.' };
                 }
             });
-
     },
 
-    getFlowers: ()=>{
-
-    	let sql = 'select * from imagequiz.flower';
-  
-    	return pool.query(sql)
-    		.then(x =>{
-    				// if (x.rows.length == 0){
-    				// 	console.log('hit');
-    				// 	console.log('Start to loop flowers');
-    				// 	flowers.flowers.map((each, index) => {
-    				// 		//console.log('index'+index);
-    				// 		//console.log('name'+each.name);
-    				// 		let qid = index;
-    				// 		let name = each.name;
-    				// 		let picture = each.picture;
-    				// 		pool.query(`insert into imagequiz.flowers (name, picture) values ($1, $2)`, [name, picture]);
-    				// 	})
-    				// //	let sql2 = "insert into imagequiz.flowers (qid, name, picture) values (1, 'Acacia','https://habahram.blob.core.windows.net/flowers/acacia.jpg' )";
-    				// //	pool.query(sql2);
-    				// }
-
-
-    				if (x.rows.length > 0){
-    			//		console.log(x.rows.length);
-    					console.log(x.rows);
-    					return {result:x.rows};
-    				}
-    			//	console.log(flowers.flowers)
-    			//	console.log(x);
-    				
-    			
-    		});
-    },
-
-    getQuiz: (name) => {
-        let sqlQuery = `select q.id as quiz_id, q2.*  from imagequiz.quiz q join imagequiz.quiz_question qq on q.id = qq.quiz_id 
-        join imagequiz.question q2 on qq.question_id = q2.id 
-        where lower(q.name) =  $1`;
-        return pool.query(sqlQuery, [name.toLowerCase()])
+    getQuiz: (quizName) => {
+        let query = `
+        select q1.id as quiz_id, q3.* from imagequiz.quiz as q1 
+        join imagequiz.quiz_question as q2 on q1.id = q2.quiz_id
+        join imagequiz.question q3 on q2.question_id = q3.id 
+        where lower(q1.name) = $1`;
+        return pool.query(query, [quizName.toLowerCase()])
             .then(x => {
-                //console.log(x);
                 let quiz = {};
                 if (x.rows.length > 0) {
                     quiz = {
@@ -94,35 +61,60 @@ let store = {
                 return quiz;
             });
     },
+    
+    
+    postScore: (quizTaker, quizName, score, date) => {
+        let idQuery = `select c.id as customer_id from imagequiz.customer c where c.email = $1`;
+        //let customer_id;
+        pool.query(idQuery, [quizTaker])
+            //getCustomerId(quizTaker)
+            .then(x => {
+                if (x.rows.length > 0) {
+                let customer_id = x.rows[0].customer_id;
+                let quizIdQuery = `select q.id as quiz_id from imagequiz.quiz q where lower(q.name) = $1`;
+                //let quiz_id;
+                pool.query(quizIdQuery, [quizName.toLowerCase()])
+                    //getQuizId(quizName)
+                    .then(y => {
+                        //if (y.rows.length > 0) {
+                        let quiz_id = y.rows[0].quiz_id;
+                        let query = `
+                                    insert into imagequiz.score (customer_id, quiz_id, date, score)
+                                    values ($1, $2, $3, $4)`;
+                        return pool.query(`insert into imagequiz.score (customer_id, quiz_id, date, score) values ($1, $2, $3, $4)`, [Number(customer_id), Number(quiz_id), date, score])
+                    })
+                    .catch(e => {
+                        console.log(e);
+                        //return undefined;
+                    })
+                } else {
+                    console.log('customer not in database');
+                    return {done: false, message: 'customer not in database'};
+                }
+            })
+            .catch(e => {
+                console.log(e);
+                return undefined;
+            })
+    }, 
 
-    addScore:(quizTaker, quizName, scores) =>{
-    	
-
-    	// return pool.query(`insert into imagequiz.quiz`.[quizTaker, quizName, scores])
-    	// 	.then()
-        return pool.query('select * from imagequiz.flowers');
-
+    getScore: (quizTaker, quizId) => {
+        let query = `
+        select s.score from imagequiz.customer c
+        join imagequiz.score s on s.customer_id = c.id
+        join imagequiz.quiz q on q.id = s.quiz_id
+        where c.email = $1 
+        and lower(q.name) = $2`;
+        return pool.query(query, [quizTaker, quizId.toLowerCase()])
+       
     },
 
-    getScores: () => {
-        return scores;
-    },
-    insertQuestion: () => {
-        let result = [];
-        for (id in quizzes) {
-            for (el of quizzes[id].questions) {
-                result.push([el.picture, el.choices.join(','), el.answer])
-            }
-        }
-        return pool.query(format('insert into imagequiz.question (picture, choices, answer) values %L',result), [], (err, result2) =>{
-            console.log(err);
-            console.log(result2);
-        })
-        //return pool.query('insert into imagequiz.question (picture, choices, answer) values ($1,$2,$3),($4,$5,$6),($7,$8,$9),($10,$11,$12),($13,$14,$15),($16,$17,$18),($19,$20,$21),($22,$23,$24),($25,$26,$27),($28,$29,$30),($31,$32,$33),($34,$35,$36),($37,$38,$39),($40,$41,$42),($43,$44,$45),($46,$47,$48),($49,$50,$51),($52,$53,$54),($55,$56,$57),($58,$59,$60),($61,$62,$63),($64,$65,$66),($67,$68,$69),($70,$71,$72),($73,$74,$75),($76,$77,$78),($79,$80,$81),($82,$83,$84),($85,$86,$87),($88,$89,$90),($91,$92,$93),($94,$95,$96),($97,$98,$99),($100,$101,$102),($103,$104,$105),($106,$107,$108),($109,$110,$111),($112,$113,$114),($115,$116,$117),($118,$119,$120),($121,$122,$123),($124,$125,$126),($127,$128,$129),($130,$131,$132),($133,$134,$135),($136,$137,$138),($139,$140,$141),($142,$143,$144),($145,$146,$147),($148,$149,$150),($151,$152,$153),($154,$155,$156),($157,$158,$159),($160,$161,$162)', result)
-        //note my attempt was rough but using format from pg package works better
-        //var format = require('pg-format'); //this is required to use pg format()
+    getFlowers: () => {
+        let query = `
+        select f.name, f.picture from imagequiz.flower f`;
+        return pool.query(query, [])
     }
 
 }
 
-module.exports = { store }
+module.exports = { store };
--- index.js
+++ index.js
@@ -1,129 +1,187 @@
 //dependencies
 const express = require('express');
 var cors = require('cors');
+var passport = require('passport');
+var LocalStrategy = require('passport-local');
+var session = require('express-session');
+var SQLiteStore = require('connect-sqlite3')(session);
+
 const { store } = require('./data_access/store');
 
 
 const application = express();
 const port = process.env.PORT || 4000 ;
 
-// middlewares
-application.use(cors());
-application.use(express.json());
+//middlewares
+app.use(cors({
+    origin: "https://kenny48620.github.io",
+    credentials: true
+}));
+app.use(express.json());
 
 
-// methods
-application.get('/', (request, response) => {
-
-    store.insertQuestion();
- 
-    response.status(200).json({ done: true, message: 'Hey! Welcome to hello world backend API2!' });
-});
-
-// register
-application.post('/register', (request, response) => {
-    let name = request.body.name;
-    let email = request.body.email;
-    let password = request.body.password;
-    console.log("========== Test =============")
-    console.log("=========="+ name +"=============")
-    console.log("========== Test =============")
-    store.addCustomer(name, email, password)
-    .then(x => response.status(200).json({ done: true, message: 'The customer was added successfully!' }))
-    .catch(e => {
-        console.log(e);
-        response.status(500).json({done: false, message: 'The customer was not added due to an error.'});
-    });
-    
-});
-
-// login
-application.post('/login', (request, response) => {
-    let email = request.body.email;
-    let password = request.body.password;
-    store.login(email, password)
-    .then(x => {
-        if(x.valid) {
-            response.status(200).json({ done: true, message: 'The customer logged in successfully!' });
-        } else {
-            response.status(401).json({ done: false, message: x.message });
-        }
-    })
-    .catch(e => {
-        console.log(e);
-        response.status(500).json({done: false, message: 'Something went wrong.'});
-    });
-    
-});
-
-// flowers
-application.get('/flowers', (request, response) =>{
-	store.getFlowers()
-	.then( x => {
-		if (x.result){
-			response.status(200).json({ done: true, result: x.result, message: 'Get the flowers successfully!' });
-		}else{
-			response.status(401).json({ done: false, message: 'Something went wrong as getting the flowers!'});	
-		}
-
-	})
-	.catch(e => {
-		console.log('///////// Errors in /flowers /////////');
-		response.status(500).json({done: false, message: 'Something went wrong in getFlowers().'});
-	})
-
-});
-
-// /quiz/:id 
-application.get('/quiz/:name', (request, response) => {
-    let name = request.params.name;
-    store.getQuiz(name)
-    .then(x => {
-       if(x.id) {
-        response.status(200).json({ done: true, result: x });
-       } else {
-        response.status(404).json({ done: false, message: result.message });
-       }
-    })
-    .catch(e => {
-        console.log(e);
-        response.status(500).json({done: false, message: 'Something went wrong.'});
-    })    
-});
 
 
-application.post('/score', (request, response) => {
-    let quizTaker = request.body.quizTaker;
-    let quizName  = request.body.quizName;
-    let scores    = request.body.scores;
-    console.log('====== in /score =======');
+app.use((request, response, next) => {
+    console.log(`request url: ${request.url}`);
+    console.log(`request method: ${request.method}`);
+    //only for development. Remove the next two lines when you deploy your final version.
+    console.log(`request body:`);
     console.log(request.body);
+    next();
+})
 
-    store.addScore(quizTaker, quizName, scores)
-    .then(x => {
+passport.use(
+    new LocalStrategy({ usernameField: 'email' }, function verify(username, password, cb) {
+        store.login(username, password)
+            .then(x => {
+                if (x.valid) {
+                    return cb(null, x.user);
+                } else {
+                    return cb(null, false, { message: 'Incorrect username or password.' });
+                }
+            })
+            .catch(e => {
+                console.log(e);
+                cb('Somethign went wrong!');
+            });
 
-    })
-    .catch(e =>{
-        console.log(e);
-        response.status(500).json({done: false, message: 'Something went wrong in post score.'});
-    })
+    }));
 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new SQLiteStore({ db: 'sessions.db', dir: './sessions' })
+}));
+app.use(passport.authenticate('session'));
 
+passport.serializeUser(function (user, cb) {
+    process.nextTick(function () {
+        cb(null, { id: user.id, username: user.username });
+    });
+});
+
+passport.deserializeUser(function (user, cb) {
+    process.nextTick(function () {
+        return cb(null, user);
+    });
+});
+
+//default root
+app.get('/', (req, res) => {
+    res.status(200).json({ done: true, message: 'This is the backend for imagequiz 2' })
+});
+
+//1
+app.post('/register', (req, res) => {
+    let name = req.body.name;
+    let email = req.body.email;
+    let password = req.body.password; /* store will handle encryption */
+    store.addCustomer(name, email, password)
+        .then(x => {
+            //console.log(x);
+            res.status(200).json({ done: true, message: 'A customer has been added successfully' })
+        })
+        .catch(e => {
+            console.log(e);
+            res.status(500).json({ done: false, message: 'Customer not added due to an error.' });
+        });
 
 });
-// /scores/:quiztaker/:quizname
-// application.get('/scores/:quiztaker/:quizname', (request, response) => {
-//     let quizTaker = request.params.quiztaker;
-//     let quizName = request.params.quizName;
-//     let scores = store.getScores(quizTaker, quizName);
-//     response.status(200).json({ done: true, result: scores });
-    
-// });
 
+//2
+//app.post('/login', (req, res) => {
+//    let email = req.body.email;
+//    let password = req.body.password;
+//    store.login(email, password)
+//        .then(x => {
+//            if (x.valid) {
+//                res.status(200).json({ done: true, message: 'Logged in successfully' });
+//            } else {
+//                res.status(401).json({ done: false, message: x.message });
+//            }
+//        });
+//});
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/login/succeeded',
+    failureRedirect: '/login/failed'
+}));
+app.get('/login/succeeded', (request, response) => {
+    response.status(200).json({ done: true, message: 'The customer logged in successfully.' });
+});
 
+app.get('/login/failed', (request, response) => {
+    response.status(401).json({ done: false, message: 'The credentials are not valid.' });
+});
 
+//3
+app.get('/flowers', (req, res) => {
+    store.getFlowers()
+        .then(x => {
+            console.log(x.rows);
+            res.status(200).json({ done: true, result: x.rows, message: 'These are the flowers from flowers.js going through the api' });
+        })
 
+});
 
-application.listen(port, () => {
-    console.log(`Listening to the port ${port} `);
+//4
+app.get('/quiz/:name', (req, res) => {
+    //stop users from accessing quiz if not logged in
+    if (!req.isAuthenticated()) {
+        res.status(401).json({done: false, message: 'Please sign in first.'})
+    }
+    let quizName = req.params.name;
+    //res.status(200).json({done: true, result: quizzes[Name], message: 'Here is the quiz'});
+    store.getQuiz(quizName)
+        .then(x => {
+            if (x.id) {
+                res.status(200).json({ done: true, result: x });
+            } else {
+                res.status(404).json({ done: false, message: x.message });
+            }
+        })
+        .catch(e => {
+            console.log(e);
+            res.status(500).json({ done: false, message: 'Something went wrong.' });
+        })
+});
+
+//5
+app.post('/score', (req, res) => {
+    let quizTaker = req.body.quizTaker;
+    let quizName = req.body.quizName;
+    let score = req.body.score;
+    let date = new Date();
+    date = date.toDateString();
+    //scores.push({ quizTaker: quizTaker, quizName: quizName, score: score, date: date });
+    //console.log('scores: ', scores);
+    store.postScore(quizTaker, quizName, score, date)
+    res.status(200).json({ done: true, message: 'score added' });
+
+});
+
+//6
+app.get('/scores/:quiztaker/:quizid', (req, res) => {
+    let quizTaker = req.params.quiztaker;
+    let quizId = req.params.quizid;
+    //console.log('quizid: ', quizId);
+    store.getScore(quizTaker, quizId)
+        .then(x => {
+            //console.log('x', x.rows.length);
+            if (x.rows.length > 0) {
+                res.status(200).json({ done: true, result: x.rows, message: 'The score was found' });
+            } else {
+                res.status(404).json({ done: false, result: undefined, message: 'The score was not found' });
+            }
+        })
 })
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
+
+app.post('/logout', (req,res) => {
+    req.logout();
+    res.redirect('/');
+});
